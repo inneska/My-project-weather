32ee93f0234e39e1bc8b323c092eba1409adc639
inneska <Inneska696@gmail.com>
2023-10-06 00:21:40 +0300

    add automatic change icon

--- src/index.js
+++ src/index.js
@@ -34,6 +34,14 @@ function showWeatherCondition(response) {
   //
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  //
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  //
+  iconElement.setAttribute("alt", description);
 }
 
 function DefaultCity(city) {
